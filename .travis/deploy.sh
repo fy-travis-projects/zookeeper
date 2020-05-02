b39aaa5bfd2b750587286ea543b040e1d58751d0
zfy <rachmanino@yahoo.com>
2020-05-02 18:01:07 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -31,5 +31,5 @@ cd $HOME
 pwd
 ls -al
 
-rsync -W -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 lib travis@35.236.128.26:/home/travis/projects/$name/
-rsync -W -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 project travis@35.236.128.26:/home/travis/projects/$name/
+rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 lib travis@35.236.128.26:/home/travis/projects/$name/
+rsync -W -rav -e "ssh -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 project travis@35.236.128.26:/home/travis/projects/$name/
